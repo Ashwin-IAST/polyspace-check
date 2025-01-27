2ee92f7fc73fdbe9adfe1702141c2abde7489c4d
Ashwin-IAST <ashwin.krishna@iast-software.com>
2025-01-27 10:28:50 +0530

    Create throttle_control.h

---
+++ include/throttle_control.h
@@ -0,0 +1,7 @@
+#ifndef THROTTLE_CONTROL_H
+#define THROTTLE_CONTROL_H
+
+// Function to simulate throttle adjustment logic
+void adjust_throttle(float current_speed, float target_speed);
+
+#endif // THROTTLE_CONTROL_H
